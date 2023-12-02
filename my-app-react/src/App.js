23709faa5c2469e59ed42832dafa2a54c70b3d49
Celestx2z <142236760+Celestx2z@users.noreply.github.com>
2023-12-02 15:48:18 +0100

    last commit with the error 500 Internal Server Error

--- my-app-react/src/App.js
+++ my-app-react/src/App.js
@@ -1,23 +1,59 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
 import './App.css';
-import { Login } from './components/Login';
-import { Register } from './components/Register'; 
+import Login from './components/Login';
 
-function App() {
-  const [currentForm, setCurrentForm] = useState('login');
 
-  const toggleForm = () => {
-    setCurrentForm(currentForm === 'login' ? 'register' : 'login');
+export default function App() {
+  const [token, setToken] = useState(null);
+  const [sellerProfile, setSellerProfile] = useState(null)
+
+  const handleLogin = (newToken, profile) => {
+    setToken(newToken);
+    setSellerProfile(profile);
   };
 
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+    <div>
+      {!token ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <div>
+          <div>
+            <h1>Billet Réduc</h1>
+            <Post apiUrl="http://localhost:1337/api/billet-reducs" />
+          </div>
+  
+          <div>
+            <h1>France Billet</h1>
+            <Post apiUrl="http://localhost:1337/api/france-billets" />
+          </div>
+  
+          <div>
+            <h1>Mister Good Deal</h1>
+            <Post apiUrl="http://localhost:1337/api/mister-good-deals" />
+          </div>
+  
+          <div>
+            <h1>Nature & Découverte</h1>
+            <Post apiUrl="http://localhost:1337/api/nature-and-decouvertes" />
+          </div>
+  
+          <div>
+            <h1>PC Clinic</h1>
+            <Post apiUrl="http://localhost:1337/api/pc-clinics" />
+          </div>
+  
+          <div>
+            <h1>Vanden Borre</h1>
+            <Post apiUrl="http://localhost:1337/api/vanden-borres" />
+          </div>
+  
+          <div>
+            <h1>WeFix</h1>
+            <Post apiUrl="http://localhost:1337/api/we-fixes" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
--- my-app-react/src/components/Login.jsx
+++ my-app-react/src/components/Login.jsx
@@ -1,25 +1,84 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import '../Login.css';
 
-export const Login = (props) => {
-  const[email, setEmail] = useState('');
-  const[pass, setPass] = useState('');
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();  
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-console.log(email);
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch('http://localhost:1337/api/auth/local', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          identifier: email,
+          password,
+        }),
+      });
+  
+      if (!response.ok) {
+        console.error("Authentication Error", response);
+        return;
+      }
+  
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
+        const data = await response.json();
+        console.log(data);
+  
+        props.onLogin(data.jwt, data.user.username, data.user.sellerStock);
+  
+        if (data.jwt) {
+          if (data.user.role === 'SellerFnac') {
+            navigate("/SellerFnacPage");
+          } else if (data.user.role === 'AutreRole') {
+            navigate("/AutrePage");
+          }
+        }
+      } else {
+        console.error("Non-JSON Response. Content-Type:", contentType);
+      }
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+  };
+  
+
+
+
 
   return (
-    <div className="auth-form-container">
-        <h2>Login</h2>
-        <form className="login-form" onSubmit={handleSubmit} >
-          <label htmlFor="email">Email</label>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-          <label htmlFor="password">Password</label>
-          <input value={pass} onChange={(e) => setPass(e.target.value)}  type="password" placeholder="**********" id="password" name="email" />
-          <button type="submit">Login</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
+    <div>
+      <h2>Connection</h2>
+      <form onSubmit={handleSubmit}>
+        <label className="Identifiant">
+          Identifiant:
+          <input 
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </label>
+        <br />
+        <label className="Password">
+          Password :
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default Login;
